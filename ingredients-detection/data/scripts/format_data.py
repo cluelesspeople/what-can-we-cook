51ec0e16db6e184562f5cc9867f854f867f0208d
Khavin Shankar <khavinshankar@gmail.com>
2021-07-05 21:01:20 +0530

    added a script to format datasets

---
+++ ingredients-detection/data/scripts/format_data.py
@@ -0,0 +1,40 @@
+import os
+import json
+
+json_file = "../aladdinss.json"
+images_dir = "../aladdinss"
+labels_dir = "../aladdinss"
+save_dir = "../khavin"
+
+with open(json_file) as file:
+  data_info = json.load(file)
+
+exts = [".jpg", ".jpeg", ".png", ".webp"]
+
+def readAndSave(file_name, class_name, i, img_read_loc=images_dir, txt_read_loc=labels_dir, save_loc=save_dir):
+    ext = 0
+    while True:
+        try:
+            with open(f"{img_read_loc}/{file_name}{exts[ext]}",'rb') as image_file:
+                image_string = image_file.read()
+                with open(f"{save_loc}/images/{class_name}/{class_name}-{i}.jpg",'wb') as dest_image:
+                    dest_image.write(image_string)
+            with open(f"{txt_read_loc}/{file_name}.txt",'r') as label_file:
+                label_string = label_file.read()
+                with open(f"{save_loc}/labels/{class_name}/{class_name}-{i}.txt",'w') as dest_label:
+                    dest_label.write(label_string)
+                    break
+        except FileNotFoundError:
+            if ext == 3: 
+                print("File Not Found!")
+                break;
+            ext += 1
+
+for class_name, file_names in data_info.items():
+    os.makedirs(f"{save_dir}/images/{class_name}")
+    os.makedirs(f"{save_dir}/labels/{class_name}")
+    offset = len(os.listdir(f"{save_dir}/images/{class_name}")) + 1
+    for i, file_name in enumerate(file_names):
+        readAndSave(file_name, class_name, i + offset)
+
+
