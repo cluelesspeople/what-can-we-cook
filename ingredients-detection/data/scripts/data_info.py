b8bda38065ec3deda6c87a50b752ea83aa71598b
Khavin Shankar <khavinshankar@gmail.com>
2021-07-05 14:13:13 +0530

    added a script to create a json file with classes containg respective image_file names

---
+++ ingredients-detection/data/scripts/data_info.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+os.chdir("../aladdinss")
+files = os.listdir()
+
+record = {}
+for file in files:
+    if file.__contains__(".txt"):
+        if file == "classes.txt": continue
+
+        file_object = open(file, "r")
+        content = file_object.read()
+        for bbox in content.strip().split("\n"):
+            [class_id, *bbox_info] = bbox.split(" ")
+            if class_id in record:
+                record[class_id].add(file.replace(".txt", ""))
+            else:
+                record[class_id] = set({file.replace(".txt", "")})
+
+def convert_set_to_list(obj): 
+    # obj = {1: set(), 2: set(),}
+    new_obj = {}
+    for key, value in obj.items():
+        new_obj[key] = list(value)
+    return new_obj
+
+os.chdir("..")
+with open('aladdinss.json', 'w') as file:
+    json.dump(convert_set_to_list(record), file)
